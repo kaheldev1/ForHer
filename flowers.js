4163f81deb6d31c1a1e204d3c99de5fc4d04eb1a
kaheldev1 <keltilaon8@gmail.com>
2025-06-09 12:33:28 +0800

    Update flowers.js

--- flowers.js
+++ flowers.js
@@ -4,3 +4,24 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+document.addEventListener('DOMContentLoaded', function() {
+    const backgroundMusic = document.getElementById('backgroundMusic');
+
+    if (backgroundMusic) {
+        function tryPlayMusic() {
+            backgroundMusic.play()
+                .then(() => {
+                    document.removeEventListener('click', tryPlayMusic);
+                    document.removeEventListener('keypress', tryPlayMusic);
+                })
+                .catch(error => {
+                    console.error("Autoplay prevented:", error);
+                    document.addEventListener('click', tryPlayMusic);
+                    document.addEventListener('keypress', tryPlayMusic);
+                });
+        }
+
+        tryPlayMusic();
+    }
+});
